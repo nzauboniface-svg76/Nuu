8bf2826d18d5fb55468c831349647c333c153169
Bony Born <ezekielbon2@gmail.com>
2025-09-17 08:25:52 +0000

    save order to real db

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from app import views
 
 # Create your models here.
+
 class CartOrder(models.Model):
-    customer=models.CharField(max_length=100)
-    email=models.EmailField()
-    address=models.CharField(max_length=100)
-    payment=models.DecimalField(max_digits=10, decimal_places=2, blank=True)
-    items=models.CharField(max_length=100)
-    timestamp=models.DateTimeField(auto_now_add=True)
+    customer = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    payment = models.CharField(max_length=50)  # Store payment method name
+    items = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.customer}"
